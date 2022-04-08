6c280bd8a75d79551e667337bd50491a48edb39b
Nikhil Nagrale <nikhil.nagrale@practo.com>
2022-04-09 01:43:48 +0530

    refractoring code

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,46 +1,34 @@
-import SelectedWeatherCard from './SelectedWeatherCard';
 import './Weather.css';
 import { useEffect, useState } from 'react';
-import useFetchApi from '../Hooks/useFetchApi'
+import HourlyCard from './HourlyCard'
 
 const Weather = () => {
-    // const BASE_URL = "http://api.openweathermap.org/geo/1.0/direct?q=Nagpur,91&limit=1&appid=31d3abaf0f1303eb267a9bc704c77edc"
-    const [ data , setData ] = useState({});
-    const [ dailyData, setDailyData ] = useState(null);
-    const [latLon, setLatLon] = useState({lat:0, lon:0});
+    const [data, setData] = useState(null);
+    const [dailyData, setDailyData] = useState(null);
+    const [latLon, setLatLon] = useState({ lat: 0, lon: 0 });
     const [city, setCity] = useState("Nagpur");
-
-    useEffect(()=>{
-        handleSubmit();
-    })
+    const [day, setDay] = useState("Monday");
 
     const handleSubmit = async (e) => {
-        e.preventDefault();
         let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},91&limit=1&appid=31d3abaf0f1303eb267a9bc704c77edc`;
         await fetch(url)
-        .then((res) => res.json())
-        .then((datas) => setLatLon({lat: datas[0].lat, lon:datas[0].lon}))
-        .catch((err) => console.log(`Error: ${err}`));
-        // api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}
+            .then((res) => res.json())
+            .then((datas) => setLatLon({ lat: datas[0].lat, lon: datas[0].lon }))
+            .catch((err) => console.log(`Error: ${err}`));
+
         url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latLon.lat}&lon=${latLon.lon}&exclude=hourly,minutely,alerts,current&appid=31d3abaf0f1303eb267a9bc704c77edc&units=metric`;
         await fetch(url)
-        .then((res) => res.json())
-        .then((datas) => setDailyData(datas))
-        .catch((err) => console.log(`Error: ${err}`));
+            .then((res) => res.json())
+            .then((datas) => setDailyData(datas))
+            .catch((err) => console.log(`Error: ${err}`));
 
-        // console.log(dailyData);
-        
         url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latLon.lat}&lon=${latLon.lon}&appid=31d3abaf0f1303eb267a9bc704c77edc&units=metric`;
         await fetch(url)
-        .then((res) => res.json())
-        .then((datas) => setData(datas))
-        .catch((err) => console.log(`Error: ${err}`));
-
-        // console.log(dailyData);
+            .then((res) => res.json())
+            .then((datas) => setData(datas))
+            .catch((err) => console.log(`Error: ${err}`));
     }
 
-
-    // http://api.openweathermap.org/geo/1.0/direct?q=Nagpur,91&limit=1&appid=31d3abaf0f1303eb267a9bc704c77edc
     return (
         <div className='WeatherApp'>
             <h1>Weather ForeCast</h1>
@@ -48,31 +36,43 @@ const Weather = () => {
             <div className='inputBox'>
                 <div className='searchBar'>
                     <div class="icons8-search"></div>
-                    <input value={city} onChange={(e)=>setCity(e.target.value)} className='searchBox' type="text" placeholder='Type Your City Name'/>  
+                    <input value={city} onChange={(e) => setCity(e.target.value)} className='searchBox' type="text" placeholder='Type Your City Name' />
                 </div>
                 <div className='submitSearch'>
-                    <button class="submitBtn" onClick={handleSubmit}>SEARCH</button>
+                    <button class="submitBtn" onClick={() => { handleSubmit(); }}>SEARCH</button>
                 </div>
             </div>
-            
+
             <div className='fiveDayWeather'>
                 {
-                   dailyData && dailyData.daily.map((value, index)=>
-                    (<div className='weatherCard'>
-                        <p>{new Date(value.dt * 1000).toLocaleDateString("en", {
-						    weekday: "long",
-					    })}</p>
-                        <p>{value.temp.max}</p>
-                        <img src={`http://openweathermap.org/img/wn/${value.weather[0].icon}@2x.png`} alt="" />
-                        <p>{value.temp.min}</p>
-                        <p>{value.weather[0].description}</p>
-                    </div>)
+                    dailyData && dailyData.daily.map((value, index) =>
+                        index < 7 ? (<div key={index} onClick={() => {
+                            setDay(new Date(value.dt * 1000).toLocaleDateString("en", {
+                                weekday: "long",
+                            }));
+                        }
+                        } className='weatherCard'>
+                            <p>{new Date(value.dt * 1000).toLocaleDateString("en", {
+                                weekday: "long",
+                            })}</p>
+                            <p>{value.temp.max}</p>
+                            <img src={`http://openweathermap.org/img/wn/${value.weather[0].icon}@2x.png`} alt="" />
+                            <p>{value.temp.min}</p>
+                            <p>{value.weather[0].description}</p>
+                        </div>) : null
                     )
                 }
             </div>
 
-            <div className='SelectedOne'>
-                {/* <SelectedWeatherCard /> */}
+            <div className="threeHourTemp">
+                {
+                    data && data.list.map((value, index) => day === new Date(value.dt * 1000).toLocaleDateString("en", {
+                        weekday: "long",
+                    }) ? (
+                        <HourlyCard value={value} />
+                    ) : null
+                    )
+                }
             </div>
         </div>
     );
