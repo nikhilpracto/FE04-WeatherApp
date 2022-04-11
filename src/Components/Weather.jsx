b4819a63d7f7cc65a827ecb7eadb4c677733c1a9
Nikhil Nagrale <nikhil.nagrale@practo.com>
2022-04-11 21:36:39 +0530

    fix bugs

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -9,24 +9,52 @@ const Weather = () => {
     const [city, setCity] = useState("Nagpur");
     const [day, setDay] = useState("Monday");
 
-    const handleSubmit = async (e) => {
+    const callLatLon = async () => {
         let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},91&limit=1&appid=31d3abaf0f1303eb267a9bc704c77edc`;
         await fetch(url)
             .then((res) => res.json())
-            .then((datas) => setLatLon({ lat: datas[0].lat, lon: datas[0].lon }))
+            .then((datas) => setLatLon({ ...latLon, lat: datas[0].lat, lon: datas[0].lon }))
             .catch((err) => console.log(`Error: ${err}`));
-
-        url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latLon.lat}&lon=${latLon.lon}&exclude=hourly,minutely,alerts,current&appid=31d3abaf0f1303eb267a9bc704c77edc&units=metric`;
+    }
+    
+    const callDailyData = async () => {
+        let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latLon.lat}&lon=${latLon.lon}&exclude=hourly,minutely,alerts,current&appid=31d3abaf0f1303eb267a9bc704c77edc&units=metric`;
         await fetch(url)
             .then((res) => res.json())
             .then((datas) => setDailyData(datas))
             .catch((err) => console.log(`Error: ${err}`));
 
-        url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latLon.lat}&lon=${latLon.lon}&appid=31d3abaf0f1303eb267a9bc704c77edc&units=metric`;
+    }
+
+    const callHourData = async () => {
+        let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${latLon.lat}&lon=${latLon.lon}&appid=31d3abaf0f1303eb267a9bc704c77edc&units=metric`;
         await fetch(url)
             .then((res) => res.json())
             .then((datas) => setData(datas))
             .catch((err) => console.log(`Error: ${err}`));
+    }
+
+    const handleSubmit = () => {
+        // let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},91&limit=1&appid=31d3abaf0f1303eb267a9bc704c77edc`;
+        // fetch(url).then((res)=>res.json()).then((datas)=>{
+        //     setLatLon({lat: datas[0].lat, lon: datas[0].lon});
+        //     let url2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${latLon.lat}&lon=${latLon.lon}&exclude=hourly,minutely,alerts,current&appid=31d3abaf0f1303eb267a9bc704c77edc&units=metric`;
+        //     return fetch(url2);
+        // }).then((res)=>res.json()).then((dailydata)=>{
+        //     setDailyData(dailydata);
+        //     let url3 = `https://api.openweathermap.org/data/2.5/forecast?lat=${latLon.lat}&lon=${latLon.lon}&appid=31d3abaf0f1303eb267a9bc704c77edc&units=metric`;
+        //     return fetch(url3);
+        // }).then((res)=>res.json()).then((hourdata)=>{
+        //     setData(hourdata);
+        // }).catch((error) =>{
+	    //     console.warn(error);
+        // });
+
+        callLatLon();
+        callDailyData();
+        callHourData();
+        
+        console.log(data, dailyData, latLon, city);
     }
 
     return (
